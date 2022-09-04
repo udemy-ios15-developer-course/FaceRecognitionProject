ab150de3e697b606028f5181af847d2095c7d652
Chris Hand <cj.hand@gmail.com>
2022-09-04 14:48:30 -0700

    Build a simple sample that will use local biometric authentication

--- FaceRecognitionProject/ViewController.swift
+++ FaceRecognitionProject/ViewController.swift
@@ -6,14 +6,42 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func signInClicked(_ sender: Any) {
+        self.errorLabel.isHidden = true
+        let authContext = LAContext()
+        
+        var error : NSError?
+        
+        if authContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            
+            authContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Please sign in with biometrics") { success, signInError in
+                if !success {
+                    DispatchQueue.main.async {
+                        self.errorLabel.isHidden = false
+                        self.errorLabel.text = "Could not sign in."
+                    }
+                    
+                    return
+                }
+                
+                DispatchQueue.main.async {
+                    self.performSegue(withIdentifier: "toSignedInVC", sender: nil)
+                }
+                
+            }
+        }
+    }
 }
 
